d0ec3425a0ca4e3211b6d39eea2fafed2a98ac4a
hepiao <hepiao@baidu.com>
2025-04-01 01:30:18 +0800

    Update context launch permissionX

--- app/src/main/java/com/permissionx/app/MainFragment.kt
+++ app/src/main/java/com/permissionx/app/MainFragment.kt
@@ -26,39 +26,64 @@ class MainFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 //        val context = context!!
         binding.makeRequestBtn.setOnClickListener {
-            PermissionX.init(this)
-                .permissions(
-                Manifest.permission.CAMERA,
+            PermissionX.init(requireContext()) {
+                it.permissions(
+                    Manifest.permission.CAMERA,
                     Manifest.permission.ACCESS_FINE_LOCATION,
                     Manifest.permission.RECORD_AUDIO,
-//                    Manifest.permission.READ_CALENDAR,
-//                    Manifest.permission.READ_CALL_LOG,
-//                    Manifest.permission.READ_CONTACTS,
-//                    Manifest.permission.READ_PHONE_STATE,
-//                    Manifest.permission.BODY_SENSORS,
-//                    Manifest.permission.ACTIVITY_RECOGNITION,
-//                    Manifest.permission.SEND_SMS,
-//                    Manifest.permission.READ_EXTERNAL_STORAGE
-                )
-                .setDialogTintColor(Color.parseColor("#1972e8"), Color.parseColor("#8ab6f5"))
-                .onExplainRequestReason { scope, deniedList, beforeRequest ->
-                    val message = "PermissionX needs following permissions to continue"
-                    scope.showRequestReasonDialog(deniedList, message, "Allow", "Deny")
+                ).setDialogTintColor(Color.parseColor("#1972e8"), Color.parseColor("#8ab6f5"))
+                    .onExplainRequestReason { scope, deniedList, beforeRequest ->
+                        val message = "PermissionX needs following permissions to continue"
+                        scope.showRequestReasonDialog(deniedList, message, "Allow", "Deny")
 //                    val message = "Please allow the following permissions in settings"
 //                    val dialog = CustomDialogFragment(message, deniedList)
 //                    scope.showRequestReasonDialog(dialog)
-                }
-                .onForwardToSettings { scope, deniedList ->
-                    val message = "Please allow following permissions in settings"
-                    scope.showForwardToSettingsDialog(deniedList, message, "Allow", "Deny")
-                }
-                .request { allGranted, grantedList, deniedList ->
-                    if (allGranted) {
-                        Toast.makeText(activity, "All permissions are granted", Toast.LENGTH_SHORT).show()
-                    } else {
-                        Toast.makeText(activity, "The following permissions are denied：$deniedList", Toast.LENGTH_SHORT).show()
                     }
-                }
+                    .onForwardToSettings { scope, deniedList ->
+                        val message = "Please allow following permissions in settings"
+                        scope.showForwardToSettingsDialog(deniedList, message, "Allow", "Deny")
+                    }
+                    .request { allGranted, grantedList, deniedList ->
+                        if (allGranted) {
+                            Toast.makeText(activity, "All permissions are granted", Toast.LENGTH_SHORT).show()
+                        } else {
+                            Toast.makeText(activity, "The following permissions are denied：$deniedList", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }
+//            PermissionX.init(this)
+//                .permissions(
+//                Manifest.permission.CAMERA,
+//                    Manifest.permission.ACCESS_FINE_LOCATION,
+//                    Manifest.permission.RECORD_AUDIO,
+////                    Manifest.permission.READ_CALENDAR,
+////                    Manifest.permission.READ_CALL_LOG,
+////                    Manifest.permission.READ_CONTACTS,
+////                    Manifest.permission.READ_PHONE_STATE,
+////                    Manifest.permission.BODY_SENSORS,
+////                    Manifest.permission.ACTIVITY_RECOGNITION,
+////                    Manifest.permission.SEND_SMS,
+////                    Manifest.permission.READ_EXTERNAL_STORAGE
+//                )
+//                .setDialogTintColor(Color.parseColor("#1972e8"), Color.parseColor("#8ab6f5"))
+//                .onExplainRequestReason { scope, deniedList, beforeRequest ->
+//                    val message = "PermissionX needs following permissions to continue"
+//                    scope.showRequestReasonDialog(deniedList, message, "Allow", "Deny")
+////                    val message = "Please allow the following permissions in settings"
+////                    val dialog = CustomDialogFragment(message, deniedList)
+////                    scope.showRequestReasonDialog(dialog)
+//                }
+//                .onForwardToSettings { scope, deniedList ->
+//                    val message = "Please allow following permissions in settings"
+//                    scope.showForwardToSettingsDialog(deniedList, message, "Allow", "Deny")
+//                }
+//                .request { allGranted, grantedList, deniedList ->
+//                    if (allGranted) {
+//                        Toast.makeText(activity, "All permissions are granted", Toast.LENGTH_SHORT).show()
+//                    } else {
+//                        Toast.makeText(activity, "The following permissions are denied：$deniedList", Toast.LENGTH_SHORT).show()
+//                    }
+//                }
         }
     }
 
